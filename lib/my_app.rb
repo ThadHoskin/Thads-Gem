2563ce592da257b06c0adaacc37f1a51c93e3f3a
Thadman <thaddy@Thads-MacBook-Pro.local>
2020-03-12 10:55:16 +1100

    updated with some new methods

--- lib/my_app.rb
+++ lib/my_app.rb
@@ -17,19 +17,27 @@ def start_basketball_app
   player_data = csv.map do |row| 
     row.to_hash 
   end
+  return player_data
+end
 
   def user_input_full_data  # this is to add to the csv file, and the options menu, then delete again. 
-    user_name + points_input + rebounds_input + assists_input + games_played
+    ["2018-2019",user_name, points_input, rebounds_input, assists_input, games_played]
   end
 
-  def add_to_csv #adding data to csv
-    user_input_full_data = CSV.parse(<<~ROWS, headers: true)
-    Year,Player,Points,Rebounds,Assists,Games Played
-    2018-2019,user_name,points_input,rebounds_input,assists_input,games_played_input
-  ROWS
-  end
-CSV.open("my_app_csv_file", "a") do |csv|
-csv << user_input_full_data     # 
+  # def add_to_csv #adding data to csv
+  #   user_input_full_data = CSV.parse(<<~ROWS, headers: true)
+  #   Year,Player,Points,Rebounds,Assists,Games Played
+  #   2018-2019,user_name,points_input,rebounds_input,assists_input,games_played_input
+  # ROWS
+  # end
+
+
+# CSV.open("../my_app_csv_file", "a") do |csv|
+# csv << user_input_full_data     
+# end
+
+def stats_getter(statement)
+  puts statement
 end
 
 
@@ -44,12 +52,11 @@ end
 
   # puts "hello world"  
 
-  #define a method for the displaying of stats, 
-  # def display_of_stats
+ 
 
   # end
 
-  # stats.nba.com/stats/commonplayerinfo/1
+  
   puts "Hey there, whats your name ?".colorize(:yellow)
     user_name = gets.chomp
   puts "Lets play some basketball #{user_name}!!! "
@@ -57,17 +64,22 @@ end
     sleep(2)
   puts "Who is your favorite basketball player out of the following?".colorize(:yellow)
   puts "-" * 45
-    sleep(1)
-
-
-  rows = []
+    sleep(2)
+def table_maker
+rows = []
   rows << ["Kevin Durant", 1]
   rows << ["Giannis Antetokounmpo", 2]
   rows << ["Lebron James", 3]
   rows << ["James Harden", 4]
   rows << ["Russell Westbrook", 5]
   table = Terminal::Table.new :rows => rows
-  puts table
+  return table
+end
+
+valid_input = false
+until valid_input 
+table = table_maker
+puts table
 
   # puts "1. Kevin Durant"
   # puts "2. Giannis Antetokounmpo"
@@ -80,60 +92,65 @@ end
   user_input = gets.chomp.to_i
   puts "-" *45
   puts "\n"
-  # start_basketball_app
+  player_data = start_basketball_app
 
   case user_input 
   when 1
+  valid_input = true
     puts "You chose Kevin Durant! Reigning finals MVP, Nice work. "
-      sleep(1.5)
-    puts "Let me have a think..."
+      sleep(2)
+    stats_getter("Let me get those stats for you...")
       puts "\n"
       sleep(2)
       print_player_detail(player_data, user_input - 1)
-    puts "Ahhh, wow !! He's had a good 2018-2019 season, yet again !"
+    # puts "Ahhh, wow !! He's had a good 2018-2019 season, yet again !"
   when 2
+  valid_input = true
     puts "You chose Giannis Antetokounmpo, the Greek Freak ! Lets go Bucks ! "
       sleep(2)
-    puts "Let me get those stats for you..."
+    stats_getter("Let me get those stats for you...")
       sleep(2)
-        #pull Giannis stats
+    #pull Giannis stats
     print_player_detail(player_data, user_input - 1)
     sleep(2)
     puts "He has been incredible, I'd reckon hes got the MVP , wouldn't you !"
   when  3
-    puts "You chose Lebron James, The Chosen One ! Good stuff. "
+  valid_input = true
+    puts "You chose Lebron James, The Chosen One, The King ! Good stuff. "
       sleep(2)
-    puts "Can never go wrong with The King! "
-    puts "Lets see how hes been doing shall we... "
+    # puts "Can never go wrong with The King! "
+    stats_getter("Let me get those stats for you...")
       sleep(2)
-        #pull lebron stats
+    #pull lebron stats
     print_player_detail(player_data, user_input - 1)
     sleep(2)
     puts "Another great year from Lebron , 33 and still got it !!"
   when  4
+  valid_input = true
     puts "You chose James Harden, the Beard, the reigning MVP ! Lets get some fouls ;)"
     sleep(2)
-    puts "Well, you know you're going to get those stats when you choose The Beard, don't ya !"
-    puts "Lets go see how he faired this season..."
-      #harden stats
+    # puts "Well, you know you're going to get those stats when you choose The Beard, don't ya !"
+    stats_getter("Let me get those stats for you...")
+    #harden stats
     print_player_detail(player_data, user_input - 1)
     sleep(2)
-    puts "Well, he may have not got another MVP , but cant say he wasn't close can we ?"
+    # puts "Well, he may have not got another MVP , but cant say he wasn't close can we ?"
   when  5
+  valid_input = true
     puts "You chose Russell Westbrook, the walking triple double ! Thunder Up Baby !!"
       sleep(2)
     puts  "Ohhhh, you've chosen the Brody, that's the one, lets see if he can get another one of those triple doubles eh ! "
       sleep(2)
-    puts "Lets check those stats of his eh ..."
-        #pull Westbrook stats
+    stats_getter("Let me get those stats for you...")
     print_player_detail(player_data, user_input - 1)
     sleep(2)
     puts  "WOW !! Another triple double season, he's one of a kind isn't he !"
 
-  else user_input != 1 || user_input != 2 || user_input != 3 || user_input != 4 || user_input != 5
-    puts "Error : choose a number between 1 and 5 please! Lets try that again":
-    start_basketball_app 
+  else # user_input != 1 || user_input != 2 || user_input != 3 || user_input != 4 || user_input != 5
+    puts "Error : choose a number between 1 and 5 please! Lets try that again"
+    # start_basketball_app 
   end
+end
 
 
   puts "-" *45
@@ -156,7 +173,7 @@ end
         puts "Ohhh , feeling lucky !"
       else
         puts "Nice! Some truth to it! "
-  end
+      end
 # end  
     sleep(2)
     puts "What about rebounds ? How many of these do you think you'd be able to get?".colorize(:yellow)
@@ -168,7 +185,7 @@ end
         puts "Feeling very lucky ! Remember, these guys are really tall"
       else  
         puts "Nice , to be honest, i'd be happy wth any !"
-  end
+      end
 # end
     sleep(2)
     puts "I suppose we better check those assists out now, eh? You reckon you could get many of these? Care to venture a guess?".colorize(:yellow)
@@ -178,8 +195,8 @@ end
           puts "Can't believe it , got a John Stockton amongst us ! "
         else 
           puts "Thats a good effort, I think we'd see some turnovers too though unfortunately ! "
-  end
-end  
+        end
+# end  
     sleep(2)
     puts "\n"
     puts "Lastly, i suppose we better check how durable you're going to be. Out of 82 games, how many do you think you'd be able to play in?".colorize(:yellow)
@@ -189,11 +206,10 @@ end
           puts "Thats not possible! Try again please.".colorize(:red) #back to start of loop please
         elsif games_played > 65 && games_played < 83
           puts "I think that would be a very good effort"
-        # elsif games_played > 82 || games_played != class.integer
-          # puts "Cmon now, an actual guess please"
+        
         else
           puts "I think any number of games played is a good effort!!"
-  end
+        end
 # end
     sleep(2)
 
@@ -202,10 +218,22 @@ end
 
 
   puts "For a newbie in the NBA, you've done pretty well, you scored #{points_input} points, collected #{rebounds_input} rebounds, dished out #{assists_input} assists, and all through this managed to play a sturdy #{games_played} games against these giants ! Nice Work #{user_name}"
-end
+
+  puts "heres you on the table".colorize(:yellow)
+
+  table << [user_name,6]
+
+  puts table
+
+  # now take you out of the array
+  puts "Actually, sorry i think i preferred the table the way it was!"
+  sleep(2)
+  table = table_maker
+  puts table
 
 # start app
 start_basketball_app
+
 
 
 #case statement
